d7b89eb5d7c73cca55d92039d21982c1e415a325
cristianmeelo <cristianmeelo@gmail.com>
2023-04-12 20:40:25 -0300

    :sparkles: feat: creating router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import React from "react"
 import { RouterProvider } from "react-router-dom"
-import { router } from "./router/router"
+import { router } from "./router/Index"
 
-function App () {
+
+
+function App() {
   return (<RouterProvider router={router} />)
-} 
+}
 
 export default App
--- src/router/Index.jsx
+++ src/router/Index.jsx
@@ -1,10 +1,11 @@
 import { createBrowserRouter } from "react-router-dom"
 import PaginaInicial from "../paginas/PaginaInicial"
+import React from "react";
 
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <PaginaInicial />,
+        element: <h1>Freelando</h1>,
         children: [],
     },
 ]);

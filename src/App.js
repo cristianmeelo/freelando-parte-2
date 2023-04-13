407da5dbc5aff12b1e6de19ee6130c143666d737
cristianmeelo <cristianmeelo@gmail.com>
2023-04-12 23:56:17 -0300

    :lipstick: feat: styled all app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react"
 import { RouterProvider } from "react-router-dom"
 import { router } from "./router/Index"
+import { ProvedorTema } from "./componentes/ProvedorTema/ProvedorTema"
+import { Estilos } from "./componentes/EstilosGlobais/Estilos"
 
 
 
 function App() {
-  return (<RouterProvider router={router} />)
+  return (
+    <ProvedorTema>
+      <Estilos />
+      <RouterProvider router={router} />
+    </ProvedorTema>
+  )
 }
 
 export default App

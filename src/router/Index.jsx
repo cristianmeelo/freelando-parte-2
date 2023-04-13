41a5de48c022fcb4c2c34423a04cff2351b010b7
cristianmeelo <cristianmeelo@gmail.com>
2023-04-12 23:36:16 -0300

    :sparkles: feat: created nested routes

--- src/router/Index.jsx
+++ src/router/Index.jsx
@@ -1,11 +1,28 @@
 import { createBrowserRouter } from "react-router-dom"
 import React from "react";
 import { Home } from "../pages/home/Index";
+import { ClientTypeSelection } from "../pages/client-type-selection/Index";
+
 
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <Home />,
-        children: [],
+        children: [
+            {
+                path: "cadastro",
+                element: <ClientTypeSelection />,
+                children: [{
+                    path: "cliente",
+                    element: <h1>Interesses</h1>
+                },
+                {
+                    path: "dados-pessoais",
+                    element: <h1>Dados pessoais</h1>,
+                },
+                {
+                    path: "concluido",
+                    element: <h1>Concluido</h1>,
+                }]
+            }],
     },
 ]);

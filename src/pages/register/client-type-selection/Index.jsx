558f9757278b7111c39ae29fe4d0af25b21597f4
cristianmeelo <cristianmeelo@gmail.com>
2023-04-13 00:21:15 -0300

    :sparkles: feat: finalized layout of ClientTypeSelection

--- src/pages/register/client-type-selection/Index.jsx
+++ src/pages/register/client-type-selection/Index.jsx
@@ -4,6 +4,7 @@ import { Col, Row } from 'react-grid-system'
 
 import client from "../../../assets/cliente.png"
 import freelancer from "../../../assets/freela.png"
+import { Link } from '../../../componentes/Link/Link'
 
 export const ClientTypeSelection = () => {
     return (
@@ -17,11 +18,27 @@ export const ClientTypeSelection = () => {
             <Row>
                 <Col md={6} sm={12}>
                     <img src={client} alt="client" />
+                    <Tipografia variante="body" componente="body">
+                        Sou cliente preciso de um freela
+                    </Tipografia>
                 </Col>
                 <Col md={6} sm={12}>
                     <img src={freelancer} alt="freelancer" />
+                    <Tipografia variante="body" componente="body">
+                        Sou freela e preciso de cliente
+                    </Tipografia>
                 </Col>
             </Row>
+            <div style={{ textAlign: "center" }}>
+                <Tipografia variante="body2" componente="body2">
+                    Já tem conta?
+                </Tipografia>
+                <p>
+                    <Link variante='secundaria'>
+                        Faça login!
+                    </Link>
+                </p>
+            </div>
         </>
     )
 }

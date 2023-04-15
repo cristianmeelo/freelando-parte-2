9d1d7d014b3215066446f0f27f1bcfc058abf749
cristianmeelo <cristianmeelo@gmail.com>
2023-04-15 15:57:43 -0300

    🧱 feat: created CabecalhoCadastro component

---
+++ src/componentes/CabecalhoCadastro/Index.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+import { Tipografia } from "../../componentes/Tipografia/Tipografia"
+
+export const CabecalhoCadastro = ({ titulo, subtitulo, descricao }) => {
+    return (
+        <div style={{ textAlign: 'center' }}>
+            <Tipografia variante="h1" componente="h1">
+                {titulo}
+            </Tipografia>
+            {subtitulo && <Tipografia variante="h3" componente="h2">
+                {subtitulo}
+            </Tipografia>}
+            {descricao && <Tipografia variante="body" componente="body">
+                {descricao}
+            </Tipografia>}
+        </div>)
+}
+
